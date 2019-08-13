8e28a77476bde9121723a24a4174c8b27d46b313
ryo-as <ryo.aso@codecamp.jp>
2019-08-13 18:01:25 +0900

    [add] Add dialog sample.

--- src/script/application.es6
+++ src/script/application.es6
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Sample1 from './component/sample1.vue';
 import Sample2 from './component/sample2.vue';
 import Sample3 from './component/sample3.vue';
+import ModalSample from './component/modal-sample.vue';
 
 new Vue({
   el: "#sample1", // アプリをマウントする要素(セレクタで指定)
@@ -19,4 +20,10 @@ new Vue({
   el: "#sample3",
   components: { Sample3 },
   template: '<Sample3 />'
+});
+
+new Vue({
+  el: "#modal-sample",
+  components: { ModalSample },
+  template: '<ModalSample />'
 });
---
+++ src/script/component/modal-sample.vue
@@ -0,0 +1,69 @@
+<template lang='pug'>
+  section.container
+    h1 Dialog Sample
+    p
+      span Please click this button　→　
+      button(v-on:click='open') open modal
+    // モーダル
+    .modal(v-show='active')
+      .modal__container
+        article(v-show='page == 1')
+          h1 １ページ
+          button(v-on:click='linkTo(2)') go to 2
+        article(v-show='page == 2')
+          h1 ２ページ
+          button(v-on:click='linkTo(1)') go to 1
+          button(v-on:click='linkTo(3)') go to 3
+        article(v-show='page == 3')
+          h1 ３ページ
+          button(v-on:click='linkTo(2)') go to 2
+        button(v-on:click='close') close modal
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      active: false,
+      page: 1,
+    }),
+    methods: {
+      open () {
+        this.active = true
+      },
+      close () {
+        this.active = false
+      },
+      linkTo (page) {
+        this.page = page
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.container {
+  margin: 10px;
+  padding: 5px;
+  border: 1px solid #fff;
+}
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255,255,255, 0.3);
+}
+.modal__container {
+  display: block;
+  width: 80%;
+  height: 80%;
+  padding: 10px 20px;
+  background: #fff;
+  color: #000;
+}
+</style>
+
